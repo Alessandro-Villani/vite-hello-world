eb8e0f233e0d44adeb523e158be1f592970d3559
Alessandro-Villani <alessvill@libero.it>
2023-01-19 15:36:13 +0100

    added AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container text-center">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
